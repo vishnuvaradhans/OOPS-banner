b0b9e88c427718e72acb3b960f1826fa29aae856
vishnuvaradhans <vishnuvaradhansoff757@gmail.com>
2026-02-19 12:02:20 +0530

    Created OOPSBanner Using Stringjoin

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,4 +1,4 @@
-public class test {
+public class OOPSBanner {
     public static void main(String[] args) {
 
         System.out.println(String.join(" ", "   ***   ", "   ***    ", " ******  ", "   *****  "));
