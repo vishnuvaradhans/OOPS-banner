57a2c188b86645db099424a5037a054cfcc6f809
vishnuvaradhans <vishnuvaradhansoff757@gmail.com>
2026-02-19 12:34:52 +0530

    Created OOPSBannerApp

---
+++ OOPSBannerApp.java
@@ -0,0 +1,20 @@
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String[] lines = new String[7];
+
+        lines[0] = String.join(" ", "   ***   ", "   ***    ", " ******  ", "   *****  ");
+        lines[1] = String.join(" ", " **   ** ", " **   **  ", " *    *   ", " **       ");
+        lines[2] = String.join(" ", "**     **", "**     ** ", " ******   ", "**        ");
+        lines[3] = String.join(" ", "**     **", "**     ** ", " *        ", "  *****   ");
+        lines[4] = String.join(" ", "**     **", "**     ** ", " *        ", "      **  ");
+        lines[5] = String.join(" ", " **   ** ", " **   **  ", " *        ", "**    **  ");
+        lines[6] = String.join(" ", "   ***   ", "   ***    ", " *        ", "  *****   ");
+
+        // Loop to print banner
+        for (int i = 0; i < lines.length; i++) {
+            System.out.println(lines[i]);
+        }
+    }
+}
